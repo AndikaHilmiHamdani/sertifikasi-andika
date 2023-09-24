c3fa4cb105b7dea53c6e719b9d233ca1a7514299
AndikaHilmiHamdani <farisvodait@gmail.com>
2023-09-24 23:41:17 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'coba-apps',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
